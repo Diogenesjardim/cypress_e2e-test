7082f6b91182c986725bfd695116211c3f0c8fc8
Diógenes Jardim <diogenes.jj@gmail.com>
2023-07-14 02:41:28 -0300

    Atualização

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -92,10 +92,10 @@ Cypress.Commands.add("fillSettingsFormAndSubmit", () => {
   cy.get("#name").type("Mary Doe");
   cy.iframe(".card-field iframe")
     .as("iframe")
-    .find('[name="cardnumber"]')
+    .find("[name=\"cardnumber\"]")
     .type("4242424242424242");
-  cy.get("@iframe").find('[name="exp-date"]').type("1271");
-  cy.get("@iframe").find('[name="cvc"]').type("123");
-  cy.get("@iframe").find('[name="postal"]').type("12345");
+  cy.get("@iframe").find("[name=\"exp-date\"]").type("1271");
+  cy.get("@iframe").find("[name=\"cvc\"]").type("123");
+  cy.get("@iframe").find("[name=\"postal\"]").type("12345");
   cy.contains("button", "Purchase").click();
 });
